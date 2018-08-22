544716d62c5e733aa7e14834b695f5739981a3f4
Nicky Liu <nickyliu91@gmail.com>
2018-08-22 15:25:39 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ def get_first_name_of_season_winner(data, season)
   data.each do |tv_season, contestants|
     if tv_season == season
       tv_season.each do |item, info|
-        return tv_season["name"] if item["status"] == "Winner"
+        puts tv_season["name"] if item["status"] == "Winner"
       end
     end
   end
